b9d80e28f6103f0b718592c2b126f806f919fa89
yanlegon <yancarloslegonsuarez@gmail.com>
2024-03-15 21:47:01 -0400

    Aside changes

--- js/index.js
+++ js/index.js
@@ -1,20 +1,28 @@
-function closeChatTab(){
-    let item = document.querySelector(".chat-modal");
-    item.classList.remove("show")
-    item.classList.add("hide")
-}
-function openChatTab(){
-    let item = document.querySelector(".chat-modal");
-    item.classList.remove("hide")
-    item.classList.add("show")
-}
-function createNewTextBuble(message, sender){
-    let html = `<div class='text-buble'><strong>${sender}</strong><p>${message}</p></div>`
-    document.querySelector(".chat-load").innerHTML = html
-}
-function sendMessage(){
-    let input = document.getElementById("message");
-    createNewTextBuble(input.value, "d3ku07")
-    input.value = ""
+function selectNews(id){
+    let article_title = document.querySelector(".article-title");
+    let article_text = document.querySelector(".article-text");
+    let news_title = document.getElementById(`title${id}`);
+    let news_text = document.getElementById(`text${id}`);
 
+    if(news_title.children !== ""){
+        text = news_title.textContent
+        article_title.textContent = text
+    }
+};
+
+let childs = document.querySelector(".side-bar-page").childNodes
+for(let i = 0; i < childs.length; i++){
+    if (childs[i].innerHTML !== undefined){
+        let newChilds = childs[i].childNodes
+        for(let j=0;j<newChilds.length;j++){
+            if(newChilds[j].innerHTML !== undefined){
+                if(newChilds[j].textContent.length >= 181){
+                    console.log(newChilds[j].id)
+                    let newText = newChilds[j].textContent.slice(0, 180)
+                    console.log(newText)
+                    document.getElementById(newChilds[j].id).textContent = newText+"..."
+                }
+            }
+        }
+    }
 }
