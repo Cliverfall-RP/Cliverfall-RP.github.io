699398961fd0bebd1adea83fceb63370e47b8841
yanlegon <yancarloslegonsuarez@gmail.com>
2024-03-15 23:19:18 -0400

    Several Changes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+import news from "./contet.json" assert { type: 'json' }
+console.log()
+
 function selectNews(id){
     let article_title = document.querySelector(".article-title");
     let article_text = document.querySelector(".article-text");
@@ -6,23 +9,26 @@ function selectNews(id){
 
     if(news_title.children !== ""){
         text = news_title.textContent
+        text2 = news_text.textContent
         article_title.textContent = text
+        article_text.textContent = text2
     }
 };
-
-let childs = document.querySelector(".side-bar-page").childNodes
-for(let i = 0; i < childs.length; i++){
-    if (childs[i].innerHTML !== undefined){
-        let newChilds = childs[i].childNodes
-        for(let j=0;j<newChilds.length;j++){
-            if(newChilds[j].innerHTML !== undefined){
-                if(newChilds[j].textContent.length >= 181){
-                    console.log(newChilds[j].id)
-                    let newText = newChilds[j].textContent.slice(0, 180)
-                    console.log(newText)
-                    document.getElementById(newChilds[j].id).textContent = newText+"..."
+function formatNewsText(){
+    let childs = document.querySelector(".side-bar-page").childNodes
+    for(let i = 0; i < childs.length; i++){
+        if (childs[i].innerHTML !== undefined){
+            let newChilds = childs[i].childNodes
+            for(let j=0;j<newChilds.length;j++){
+                if(newChilds[j].innerHTML !== undefined){
+                    if(newChilds[j].textContent.length >= 181){
+                        let newText = newChilds[j].textContent.slice(0, 180)
+                        document.getElementById(newChilds[j].id).textContent = newText+"..."
+                    }
                 }
             }
         }
     }
 }
+
+formatNewsText()
